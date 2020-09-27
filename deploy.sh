9cb4c120ec4387369a17fa2ee1394919d4852f6f
harit suttisaksri <haritsuttisaksri@gmail.com>
2020-09-28 04:10:30 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ sudo cd /var/www/html
 sudo git clone https://github.com/bright72/Eventpic-backend.git
 
 sudo cd Eventpic-backend/
+sudo git pull
 sudo npm install
 sudo npm install pm2@latest -g
 sudo pm2 stop eventpic-backend
